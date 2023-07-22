ede28f0475ea8c71777bfd9336099db5f85df8a6
Ankit sharma <ankit.sharma09547@gmail.com>
2023-07-22 20:34:09 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load env variables
+const result = dotenv.config({ path: './config/config.env' });
+console.log(result);
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+console.log('#E>', process.env.NODE_ENV);
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`)
+);
