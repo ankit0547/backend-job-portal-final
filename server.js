90b0c13a35fd498d037bf8c0750e4764acae814e
Ankit sharma <ankit.sharma09547@gmail.com>
2024-02-04 14:54:22 +0530

    header fix

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ if (!fs.existsSync("./public/profile")) {
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
